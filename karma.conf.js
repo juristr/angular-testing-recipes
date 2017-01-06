9fab27b1af6c2f4a7c096c1d88fa82f704379503
Juri <juri.strumpflohner@gmail.com>
2017-01-06 18:03:47 +0100

    feat: add karma mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,8 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('angular-cli/plugins/karma'),
+      require('karma-mocha-reporter')
     ],
     files: [
       { pattern: './src/test.ts', watched: false }
@@ -31,8 +32,8 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
+              ? ['mocha', 'karma-remap-istanbul']
+              : ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
