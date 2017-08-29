f1bb0535ae7c82c2710f770b303fcdd41f46197b
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-08-29 08:25:04 +0200

    added a filter pipe and tests

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filter'
+})
+
+@Injectable()
+export class FilterPipe implements PipeTransform {
+    transform(items: any[], field: string, value: string): any[] {
+        if (!items) {
+            return [];
+        }
+        if (!field || !value) {
+            return items;
+        }
+
+        return items.filter(singleItem => singleItem[field].toLowerCase().includes(value.toLowerCase()));
+    }
+}
