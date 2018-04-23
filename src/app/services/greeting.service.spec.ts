a0046ce2169676e2f2d64f296c929b4d3a8eb5a1
Juri <juri.strumpflohner@gmail.com>
2018-04-23 22:08:42 +0200

    refactor(services): simplify testing

--- src/app/services/greeting.service.spec.ts
+++ src/app/services/greeting.service.spec.ts
@@ -4,68 +4,71 @@ import { TestBed, inject } from '@angular/core/testing';
 import { GreetingService } from './greeting.service';
 
 describe('GreetingService', () => {
-  describe('Injecting via testcase level inject', () => {
+  describe('Manually instantiating service', () => {
+    let service: GreetingService;
+
+    beforeEach(() => {
+      service = new GreetingService();
+    });
+
+    afterEach(() => {
+      service = null;
+    });
+
+    it('should greet properly', () => {
+      expect(service.sayHi('Juri')).toBe('Hi, Juri');
+    });
+  });
+
+  describe('Configuring via TestBed', () => {
     beforeEach(() => {
       TestBed.configureTestingModule({
         providers: [GreetingService]
       });
     });
 
-    it(
-      'should have a service instance',
-      inject([GreetingService], (service: GreetingService) => {
+    describe('Injecting via testcase level inject', () => {
+      it(
+        'should have a service instance',
+        inject([GreetingService], (service: GreetingService) => {
+          expect(service).toBeDefined();
+        })
+      );
+    });
+
+    describe('Injecting via test suite level inject', () => {
+      let service: GreetingService;
+
+      beforeEach(
+        inject([GreetingService], (s: GreetingService) => {
+          service = s;
+        })
+      );
+
+      it('should have a service instance', () => {
         expect(service).toBeDefined();
-      })
-    );
-  });
-
-  describe('Injecting via test suite level inject', () => {
-    let service: GreetingService;
-
-    beforeEach(() =>
-      TestBed.configureTestingModule({
-        providers: [GreetingService]
-      }));
-
-    beforeEach(
-      inject([GreetingService], (s: GreetingService) => {
-        service = s;
-      })
-    );
-
-    it('should have a service instance', () => {
-      expect(service).toBeDefined();
-    });
-  });
-
-  describe('Injecting via TestBed.get()', () => {
-    let service: GreetingService;
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [GreetingService]
-      });
-
-      service = TestBed.get(GreetingService);
-    });
-
-    it('should have a service instance', () => {
-      expect(service).toBeDefined();
-    });
-  });
-
-  describe('Testing service functions', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [GreetingService]
       });
     });
 
-    it(
-      'should greet properly',
-      inject([GreetingService], (service: GreetingService) => {
-        expect(service.sayHi('Juri')).toBe('Hi, Juri');
-      })
-    );
+    describe('Injecting via TestBed.get()', () => {
+      let service: GreetingService;
+
+      beforeEach(() => {
+        service = TestBed.get(GreetingService);
+      });
+
+      it('should have a service instance', () => {
+        expect(service).toBeDefined();
+      });
+    });
+
+    describe('Testing service functions', () => {
+      it(
+        'should greet properly',
+        inject([GreetingService], (service: GreetingService) => {
+          expect(service.sayHi('Juri')).toBe('Hi, Juri');
+        })
+      );
+    });
   });
 });
